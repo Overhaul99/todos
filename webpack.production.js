fe63ceb26d0a2d07dd400b9130f3f3cd7c5d137c
Luis <luiswester92@gmail.com>
2021-12-31 14:15:10 -0600

    Mi tercer commit

--- webpack.production.js
+++ webpack.production.js
@@ -70,11 +70,6 @@ module.exports = {
             filename: '[name].[fullhash].css',
             ignoreOrder: false,
         }),
-        new CopyPlugin({
-            patterns: [
-            { from: "src/assets/", to: "assets/" },
-            ],
-        }),
     ]
 
 }
